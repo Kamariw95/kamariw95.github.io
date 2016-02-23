cf1eede937e68111688c5c8452502d2dac962425
Kamari Wright <kamariw95@gmail.com>
2016-02-23 11:08:11 -0500

    Adds images and changes how images are included.

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,4 +2,8 @@ $(function() {
   $('.ui.fixed.button').click( function(){
     $('.ui.sidebar').sidebar('toggle');
   });
+
+  $('.item').click(function(){
+    $('.ui.sidebar').sidebar('toggle');
+  });
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,26 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-      <img class="photo" src="resources/images/nyc.jpeg"/>
+    <div class = "ui one column grid">
+      <div class ="site-sect column" id="home">
+        <!--<img class="photo" src="resources/images/nyc.jpeg"/>-->
+      </div>
+      <div class ="site-sect column" id="about">
+        <!--<img class="photo" src="resources/images/city-2.jpg"/>-->
+      </div>
+      <!--<div class ="site-sect column" id="blog">
+        <img class="photo" src="resources/images/city-7.jpg"/>
+      </div>-->
+      <div class ="site-sect column" id="project">
+        <!--<img class="photo" src="resources/images/city-6.jpg"/>-->
+      </div>
+      <div class ="site-sect column" id="photos">
+        <!--<img class="photo" src="resources/images/city-8.jpg"/>-->
+      </div>
+      <div class ="site-sect column" id="contact">
+        <!--<img class="photo" src="resources/images/city-4.jpg"/>-->
+      </div>
+    </div>
     `
 
 })
