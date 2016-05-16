caa8f2d77a7a53a004142d61ca0d0b8a81680eec
Kamari Wright <kamariw95@gmail.com>
2016-05-15 21:29:31 -0400

    adds picture

--- app/app.component.js
+++ app/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n      <div class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n    "
+                        template: "\n      <div id=\"home\" class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n      <div class =\"ui vertical segment\">\n\n      </div>\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-      <div class ="ui vertical segment">
+      <div id="home" class ="ui vertical segment">
 
       </div>
       <div class ="ui vertical segment">
