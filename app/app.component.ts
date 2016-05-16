1db2f4b9e47c59f70ba818c7e1383a28dcec98fa
Kamari Wright <kamariw95@gmail.com>
2016-05-15 20:32:24 -0400

    changes main

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,16 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-      <div class ="my-container" id="home">
+      <div class ="ui vertical segment">
+
+      </div>
+      <div class ="ui vertical segment">
+
+      </div>
+      <div class ="ui vertical segment">
+
+      </div>
+      <div class ="ui vertical segment">
 
       </div>
     `
