0a2ab62aa7c3840421c7c46c1d8ef5434a381e91
Kamariw95 <kamarimw@gmail.com>
2020-05-12 10:28:00 -0400

    deletes everrrything

--- Scripts/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.ui.fixed.button').click( function(){
-    $('.ui.sidebar').sidebar('toggle');
-  });
-
-  $('.item').click(function(){
-    $('.ui.sidebar').sidebar('toggle');
-  });
-});
